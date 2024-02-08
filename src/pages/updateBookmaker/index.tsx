2315216ff5d0aaefb960221a726adf56ffaab2f2
PardeepDuple <pradeep.duple@gmail.com>
2024-02-09 01:21:49 +0530

    match result declare from bookmaker

--- src/pages/updateBookmaker/index.tsx
+++ src/pages/updateBookmaker/index.tsx
@@ -1,6 +1,6 @@
 import { Box, Grid, Paper } from "@mui/material";
 import BetsList from "../../components/updateBookmaker/BetsList";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import BookmakerEditSection from "../../components/updateBookmaker/BookmakerEdit";
 import { useEffect } from "react";
 import {
@@ -9,11 +9,23 @@ import {
 } from "../../store/actions/addSession";
 import { AppDispatch, RootState } from "../../store/store";
 import { useDispatch, useSelector } from "react-redux";
+import { socketService } from "../../socketManager";
 
 const UpdateBookmaker = () => {
   const { state }: any = useLocation();
   const dispatch: AppDispatch = useDispatch();
+  const navigate = useNavigate();
   const { placedBets } = useSelector((state: RootState) => state.addSession);
+
+  const resultDeclared = (event: any) => {
+    try {
+      if (event?.matchId === state?.match?.id) {
+        navigate("/expert/match");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   useEffect(() => {
     if (state?.id) {
@@ -25,6 +37,7 @@ const UpdateBookmaker = () => {
         })
       );
       dispatch(getPlacedBets(state?.id));
+      socketService.user.matchResultDeclared(resultDeclared);
     }
   }, [state?.id]);
 
