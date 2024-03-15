b046542296b92b7cff336dd533c741b5f87c884d
PardeepDuple <pradeep.duple@gmail.com>
2024-03-15 11:14:05 +0530

    updates for build error for first time

--- src/routes/authRoutes.tsx
+++ src/routes/authRoutes.tsx
@@ -2,10 +2,10 @@ import { lazy } from "react";
 import { Navigate } from "react-router-dom";
 import AuthLayout from "../layout/auth";
 import Loadable from "../utils/loadable";
-import ChangePassword from "../pages/changePassword";
 import { Constants } from "../utils/Constants";
 
 const Login = Loadable(lazy(() => import("../pages/auth/login")));
+const ChangePassword = Loadable(lazy(() => import("../pages/changePassword")));
 
 const AuthRoutes = {
   path: Constants.AuthPaths.root,
