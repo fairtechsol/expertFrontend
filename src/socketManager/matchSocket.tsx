577394e091b32bcecd97dfc79fee5082bdaa6f51
PardeepDuple <pradeep.duple@gmail.com>
2024-04-11 10:39:41 +0530

    fixed error when entering any route on chrome and null check on socket

--- src/socketManager/matchSocket.tsx
+++ src/socketManager/matchSocket.tsx
@@ -2,34 +2,34 @@ import { matchSocket, socket } from ".";
 
 export const matchSocketService = {
   joinMatchRoom: (matchId: any, roleName: any) => {
-    socket.emit("matchRoom", {
+    socket?.emit("matchRoom", {
       id: matchId,
     });
 
-    matchSocket.emit("initCricketData", {
+    matchSocket?.emit("initCricketData", {
       matchId: matchId,
       roleName: roleName,
     });
   },
   leaveMatchRoom: (matchId: any) => {
-    matchSocket.emit("disconnectCricketData", {
+    matchSocket?.emit("disconnectCricketData", {
       matchId: matchId,
       roleName: "expert",
     });
   },
   leaveAllRooms: () => {
-    socket.emit("leaveAll");
+    socket?.emit("leaveAll");
   },
   matchAdded: (callback: any) => {
-    socket.on("addMatch", callback);
+    socket?.on("addMatch", callback);
   },
   getMatchRates: (matchId: string, callback: any) => {
-    matchSocket.on(`liveData${matchId}`, callback);
+    matchSocket?.on(`liveData${matchId}`, callback);
   },
   matchAddedOff: () => {
-    socket.off("addMatch");
+    socket?.off("addMatch");
   },
   getMatchRatesOff: (matchId: string) => {
-    matchSocket.off(`liveData${matchId}`);
+    matchSocket?.off(`liveData${matchId}`);
   },
 };
