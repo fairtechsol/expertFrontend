c392cc5a8ae3930cfef5dd02ae6c19ee21bf08d2
PardeepDuple <pradeep.duple@gmail.com>
2024-09-30 11:52:49 +0530

    on enter click submit function

--- src/components/matchDetails/MaxBetAdd.tsx
+++ src/components/matchDetails/MaxBetAdd.tsx
@@ -18,7 +18,8 @@ const MaxBetAdd = ({
   const [selected, setSelected] = useState<any>();
   const dispatch: AppDispatch = useDispatch();
 
-  const handleSubmit = () => {
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
     let data = {
       matchId: currentMatch?.id,
       type: matchOddsLive?.type,
@@ -36,79 +37,83 @@ const MaxBetAdd = ({
   };
   return (
     <Dialog open={open} onClose={handleClose}>
-      <DialogTitle
-        sx={{
-          textAlign: "center",
-          color: "#fff",
-          background: "linear-gradient(90deg, #004A25 5%, #FDCB52 100%)",
-          fontFamily: "Poppins, sans-serif",
-        }}
-      >
-        {title}
-      </DialogTitle>
-      <DialogContent sx={{ backgroundColor: "#fff" }}>
-        <div
-          style={{
-            width: "100%",
-            height: "80px",
-            backgroundColor: "#fff",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
+      <form onSubmit={handleSubmit}>
+        <DialogTitle
+          sx={{
+            textAlign: "center",
+            color: "#fff",
+            background: "linear-gradient(90deg, #004A25 5%, #FDCB52 100%)",
+            fontFamily: "Poppins, sans-serif",
           }}
         >
-          <TextField
-            type="number"
-            placeholder="Enter maximum bet value"
-            onChange={(e: any) => {
-              setSelected(e.target.value);
+          {title}
+        </DialogTitle>
+        <DialogContent sx={{ backgroundColor: "#fff" }}>
+          <div
+            style={{
+              width: "100%",
+              height: "80px",
+              backgroundColor: "#fff",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
             }}
-            // style={{
-            //   width: "80%",
-            //   height: "50px",
-            // }}
-          />
-        </div>
-      </DialogContent>
-      <DialogActions
-        sx={{
-          background: "linear-gradient(90deg, #004A25 5%, #FDCB52 100%)",
-        }}
-      >
-        <button
-          style={{
-            width: "25%",
-            height: "40px",
-            color: "#fff",
-            backgroundColor: "#004A25",
-            fontSize: "14px",
-            borderRadius: "5px",
-            border: "1px #004A25 solid",
-            cursor: "pointer",
-            fontFamily: "Poppins, sans-serif",
+          >
+            <TextField
+              type="number"
+              placeholder="Enter maximum bet value"
+              onChange={(e: any) => {
+                setSelected(e.target.value);
+              }}
+              // onKeyDown={(e: any) => {
+              //   if (e.key === "Enter") {
+              //     handleSubmit();
+              //   }
+              // }}
+            />
+          </div>
+        </DialogContent>
+        <DialogActions
+          sx={{
+            background: "linear-gradient(90deg, #004A25 5%, #FDCB52 100%)",
           }}
-          onClick={handleSubmit}
         >
-          Submit
-        </button>
+          <button
+            type="submit"
+            style={{
+              width: "25%",
+              height: "40px",
+              color: "#fff",
+              backgroundColor: "#004A25",
+              fontSize: "14px",
+              borderRadius: "5px",
+              border: "1px #004A25 solid",
+              cursor: "pointer",
+              fontFamily: "Poppins, sans-serif",
+            }}
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
 
-        <button
-          style={{
-            width: "25%",
-            height: "40px",
-            color: "#fff",
-            backgroundColor: "#004A25",
-            fontSize: "14px",
-            borderRadius: "5px",
-            border: "1px #004A25 solid",
-            cursor: "pointer",
-            fontFamily: "Poppins, sans-serif",
-          }}
-          onClick={handlclose}
-        >
-          Cancel
-        </button>
-      </DialogActions>
+          <button
+            style={{
+              width: "25%",
+              height: "40px",
+              color: "#fff",
+              backgroundColor: "#004A25",
+              fontSize: "14px",
+              borderRadius: "5px",
+              border: "1px #004A25 solid",
+              cursor: "pointer",
+              fontFamily: "Poppins, sans-serif",
+            }}
+            onClick={handlclose}
+          >
+            Cancel
+          </button>
+        </DialogActions>
+      </form>
     </Dialog>
   );
 };
