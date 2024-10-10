f07f3dc4594ad021adad17aff97a18ad45798e11
PardeepDuple <pradeep.duple@gmail.com>
2024-10-10 16:22:38 +0530

    fixed result status for market if match odd is not coming

--- src/components/matchDetails/CompleteMatchMarket/index.tsx
+++ src/components/matchDetails/CompleteMatchMarket/index.tsx
@@ -353,29 +353,51 @@ const CompleteMatchMarket = ({
                 }}
               ></Box>
             )}
-            {currentMatch?.resultStatus &&
-              currentMatch?.resultStatus[liveData?.id]?.status && (
-                <Box
-                  sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    width: "100%",
-                    position: "absolute",
-                    height: "100%",
-                    bottom: 0,
-                    color: "#fff",
-                    backgroundColor: "rgba(203 24 24 / 70%)",
-                  }}
-                >
-                  <Typography sx={{ color: "#fff" }}>
-                    RESULT{" "}
-                    {liveData?.stopAt || liveData?.activeStatus === "result"
-                      ? "DECLARED"
-                      : currentMatch?.resultStatus[liveData?.id]?.status}
-                  </Typography>
-                </Box>
-              )}
+            {typeof currentMatch?.resultStatus === "string" &&
+            liveData?.id &&
+            showResultBox
+              ? currentMatch?.resultStatus && (
+                  <Box
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      width: "100%",
+                      position: "absolute",
+                      height: "100%",
+                      bottom: 0,
+                      color: "#fff",
+                      backgroundColor: "rgba(203 24 24 / 70%)",
+                    }}
+                  >
+                    <Typography sx={{ color: "#fff" }}>
+                      RESULT {currentMatch?.resultStatus}
+                    </Typography>
+                  </Box>
+                )
+              : currentMatch?.resultStatus &&
+                currentMatch?.resultStatus[liveData?.id]?.status && (
+                  <Box
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      width: "100%",
+                      position: "absolute",
+                      height: "100%",
+                      bottom: 0,
+                      color: "#fff",
+                      backgroundColor: "rgba(203 24 24 / 70%)",
+                    }}
+                  >
+                    <Typography sx={{ color: "#fff" }}>
+                      RESULT{" "}
+                      {liveData?.stopAt || liveData?.activeStatus === "result"
+                        ? "DECLARED"
+                        : currentMatch?.resultStatus[liveData?.id]?.status}
+                    </Typography>
+                  </Box>
+                )}
           </Box>
         </>
       )}
