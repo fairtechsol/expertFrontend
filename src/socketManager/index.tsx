56345d98f1a044372843334c359c47741e05cd26
rohanduple <150104513+rohanduple@users.noreply.github.com>
2025-02-07 17:22:39 +0530

    feat: add match rates fetching functionality and update state management

--- src/socketManager/index.tsx
+++ src/socketManager/index.tsx
@@ -10,9 +10,9 @@ export let socket: any = null;
 export const initialiseSocket = () => {
   matchSocket = io(baseUrls.matchSocket, {
     transports: [
-      process.env.NODE_ENV === "production"
-        ? `${Constants.POLLING}`
-        : 
+      // process.env.NODE_ENV === "production"
+      //   ? `${Constants.POLLING}`
+      //   : 
         `${Constants.WEBSOCKET}`,
     ],
     auth: {
