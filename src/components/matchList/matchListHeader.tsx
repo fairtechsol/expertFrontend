82229095fb6a9c82f595c4c1d2aa79669b27309e
Pardeepfairtech <pardeepfairtech@gmail.com>
2025-04-29 17:41:08 +0530

    refactor: streamline MatchListHeader layout and improve tab handling

--- src/components/matchList/matchListHeader.tsx
+++ src/components/matchList/matchListHeader.tsx
@@ -46,112 +46,109 @@ const MatchListHeader = () => {
   };
 
   return (
-    <>
+    <Box
+      display={"flex"}
+      sx={{
+        justifyContent: "space-between",
+        px: "10px",
+        py: "10px",
+        flexDirection: { xs: "column", sm: "row" },
+      }}
+    >
       <Box
-        display={"flex"}
         sx={{
-          justifyContent: "space-between",
-          px: "10px",
-          py: "10px",
-          flexDirection: { xs: "column", sm: "row" },
+          display: "flex",
+          gap: {
+            lg: "20px",
+            xl: "20px",
+            md: "15px",
+            sm: "30px",
+            xs: "30px",
+          },
+          marginTop: "4px",
+          alignItems: "center",
         }}
       >
+        <Typography
+          sx={{
+            fontSize: { lg: "20px", md: "15px", sm: "13px", xs: "11px" },
+            color: "white",
+            fontWeight: "600",
+          }}
+        >
+          Match List
+        </Typography>
         <Box
           sx={{
+            maxWidth: { lg: "100%", md: "70%", sm: "70%", xs: "70%" },
             display: "flex",
-            gap: {
-              lg: "20px",
-              xl: "20px",
-              md: "15px",
-              sm: "30px",
-              xs: "30px",
-            },
-            marginTop: "4px",
             alignItems: "center",
           }}
         >
-          <Typography
+          <CustomTabs
+            value={selectedTab}
+            onChange={handleTabChange}
+            variant="scrollable"
+            textColor="inherit"
+            aria-label="country tabs"
+            scrollButtons
+            allowScrollButtonsMobile
             sx={{
-              fontSize: { lg: "20px", md: "15px", sm: "13px", xs: "11px" },
-              color: "white",
-              fontWeight: "600",
+              height: "30px",
+              "& .MuiTab-root": {
+                minWidth: "2.5rem",
+                minHeight: "1rem",
+                transition: "background-color 0.3s ease, color 0.3s ease",
+              },
             }}
           >
-            Match List
-          </Typography>
-          <Box
-            sx={{
-              maxWidth: { lg: "100%", md: "70%", sm: "70%", xs: "70%" },
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <CustomTabs
-              value={selectedTab}
-              onChange={handleTabChange}
-              variant="scrollable"
-              textColor="inherit"
-              aria-label="country tabs"
-              scrollButtons
-              allowScrollButtonsMobile
-              sx={{
-                height: "30px",
-                "& .MuiTab-root": {
-                  minWidth: "2.5rem",
-                  minHeight: "1rem",
-                  transition: "background-color 0.3s ease, color 0.3s ease",
-                },
-              }}
-            >
-              {gameType.map((item: any, index: any) => (
-                <Tab
-                  sx={{
+            {gameType.map((item: any, index: any) => (
+              <Tab
+                sx={{
+                  backgroundColor:
+                    selectedTab === index ? "#F8C851" : "#FFFFFF",
+                  color: "black",
+                  "&:hover": {
                     backgroundColor:
-                      selectedTab === index ? "#F8C851" : "#FFFFFF",
-                    color: "black",
-                    "&:hover": {
-                      backgroundColor:
-                        selectedTab === index ? "#E0B744" : "#F0F0F0",
-                    },
-                    height: "35px",
-                    marginTop: "4px",
-                    textAlign: "center",
-                    fontSize: { xs: "7px", sm: "8px", lg: "10px" },
-                    fontWeight: "bold",
-                  }}
-                  key={item}
-                  label={item}
-                />
-              ))}
-            </CustomTabs>
-          </Box>
-        </Box>
-
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: { xs: "space-between", sm: "flex-end" },
-          }}
-        >
-          <SearchInput
-            width="50%"
-            show={true}
-            placeholder="Search Match..."
-            handleSearch={getMatchListOnchange}
-          />
-          {(profileDetail?.allPrivilege ||
-            profileDetail?.addMatchPrivilege) && (
-            <CustomButton
-              onClick={() => {
-                navigate("/expert/add_match");
-              }}
-              title={"Add Match"}
-            />
-          )}
+                      selectedTab === index ? "#E0B744" : "#F0F0F0",
+                  },
+                  height: "35px",
+                  marginTop: "4px",
+                  textAlign: "center",
+                  fontSize: { xs: "7px", sm: "8px", lg: "10px" },
+                  fontWeight: "bold",
+                }}
+                key={item}
+                label={item}
+              />
+            ))}
+          </CustomTabs>
         </Box>
       </Box>
-    </>
+
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: { xs: "space-between", sm: "flex-end" },
+        }}
+      >
+        <SearchInput
+          width="50%"
+          show={true}
+          placeholder="Search Match..."
+          handleSearch={getMatchListOnchange}
+        />
+        {(profileDetail?.allPrivilege || profileDetail?.addMatchPrivilege) && (
+          <CustomButton
+            onClick={() => {
+              navigate("/expert/add_match");
+            }}
+            title={"Add Match"}
+          />
+        )}
+      </Box>
+    </Box>
   );
 };
 
