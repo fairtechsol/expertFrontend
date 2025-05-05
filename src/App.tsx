cb4706d975862191018def919a5f74beaa0a3c5e
Pardeepfairtech <pardeepfairtech@gmail.com>
2025-05-05 15:19:20 +0530

    refactor: enhance App component and MatchListTableHeader styling for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import routes from "./routes";
 
 function App() {
   if (process.env.NODE_ENV === "production") console.log = () => {};
-  return <RouterProvider router={routes()} />;
+  return (
+    <RouterProvider router={routes()} future={{ v7_startTransition: true }} />
+  );
 }
 
 export default App;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,5 +6,6 @@ import MainRoutes from "./mainRoutes";
 export default function routes() {
   return createBrowserRouter([AuthRoutes, MainRoutes], {
     basename: config.BASE_NAME,
+    future: { v7_relativeSplatPath: true },
   });
 }
